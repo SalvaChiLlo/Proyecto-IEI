4f9ef8f2af6fff60ccef34e3ff21e42c2e6d9504
salchil1 <salchil1@inf.upv.es>
2022-01-24 17:51:49 +0100

    last fixes

--- Launch_Project.bat
+++ Launch_Project.bat
@@ -1,2 +1,2 @@
 cd C:\Users\Administrador.WIN-2O4P6U7CI32\Desktop\Proyecto-IEI &
-npm install -g @angular/cli & npm install & start "" cmd /c front.bat & start "" cmd /c back.bat
+start "" cmd /c front.bat & start "" cmd /c back.bat
